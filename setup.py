1ee2b2a18922c7a33e40e0e75ab6b41e946daf2f
Jan <14363947+jmne@users.noreply.github.com>
2022-06-25 01:58:09 +0200

    Update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PAC-MAN-ML',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/jmne/PAC-MAN-ML',
+    license='GPL-2.0 license',
+    author='Jan',
+    author_email='14363947+jmne@users.noreply.github.com',
+    description='PAC-MAN with Machine Learning and reinforcement learning'
+)

b9c84256c74b1c2c364fa4631dcbf072099731e1
Jan <14363947+jmne@users.noreply.github.com>
2022-06-25 02:23:55 +0200

    Initialize classes

---
+++ game/Ghost.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+class Ghost(pygame.sprite.Sprite):
+
+    def __init__(self, vector):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_png('ball.png')
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.vector = vector
+
+    def update(self):
+        newpos = self.calcnewpos(self.rect,self.vector)
+        self.rect = newpos
+
+    def calcnewpos(self,rect,vector):
+        (angle,z) = vector
+        (dx,dy) = (z*math.cos(angle),z*math.sin(angle))
+        return rect.move(dx,dy)

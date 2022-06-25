2ef6dbf7daec5114776030e7931ea3d2791b4446
Jan <14363947+jmne@users.noreply.github.com>
2022-06-25 02:15:56 +0200

    Update

---
+++ game/Game.py
@@ -0,0 +1,6 @@
+import pygame
+from pygame.locals import *
+
+
+def __init__(self):
+    print("Game started..")
--- main.py
+++
@@ -1,6 +0,0 @@
-def print_hi():
-    print("Servus")
-
-
-if __name__ == '__main__':
-    print_hi()
